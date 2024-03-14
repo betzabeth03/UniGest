ecb249a7c9eb070aa52f282b7df446c236daf73c
Rafael <rafael2005perezmarin@gmail.com>
2024-03-14 09:56:06 -0400

    Agregado el script de la BD y modificado el .gitignore

--- sistemadegestionadministrativa.sql
+++ sistemadegestionadministrativa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-03-2024 a las 15:43:21
+-- Tiempo de generación: 14-03-2024 a las 14:49:49
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -39,8 +39,8 @@ CREATE TABLE `actividades` (
 --
 
 INSERT INTO `actividades` (`nombre`, `tipo`, `semana`, `id`) VALUES
-('Evaluacion escrita ', 'Examen', 0, 1),
-('Encuentro 1 ', 'Encuentro Presencial', 0, 2);
+('Encuentro 1 ', 'Encuentro Presencial', 0, 2),
+('Evaluacion e-2', 'Examen presencial', 2, 3);
 
 -- --------------------------------------------------------
 
@@ -60,8 +60,10 @@ CREATE TABLE `eventos` (
 --
 
 INSERT INTO `eventos` (`nombre`, `tipo`, `fecha`, `id`) VALUES
-('San Valentin ', 'Feriado', '2024-02-14', 1),
-('Introduccion a JavaScript', 'Conferencia ', '2024-03-20', 2);
+('Dia del Trabajador', 'Feriado', '2024-05-01', 1),
+('Introduccion a JavaScript', 'Conferencia ', '2024-03-20', 2),
+('Navidad', 'Feriado', '2024-12-25', 3),
+('Presentacion de la realid', 'Conferencia', '2024-03-20', 4);
 
 -- --------------------------------------------------------
 
@@ -79,10 +81,12 @@ CREATE TABLE `materias` (
 --
 
 INSERT INTO `materias` (`nombre`, `id`) VALUES
-('Quimica', 1),
-('Fisica', 2),
 ('Ingles', 3),
-('Programacion', 4);
+('Programacion', 4),
+('Fronted', 5),
+('backend', 6),
+('Electricidad', 10),
+('Fisica', 11);
 
 -- --------------------------------------------------------
 
@@ -101,10 +105,23 @@ CREATE TABLE `profesores` (
 --
 
 INSERT INTO `profesores` (`nombre`, `apellido`, `id`) VALUES
-('Juan', 'Torres', 1),
-('Laila', 'Aldana', 2),
-('Mary', 'Marin', 3),
-('Laura ', 'Aldana', 4);
+('Angel', 'Herrera', 2),
+('Daniela', 'Miranda', 4),
+('Maria', 'Perez', 5),
+('Jesus', 'Fernandez', 9),
+('Juan', 'Hernandez', 10),
+('Rafael', 'Caldera', 15),
+('Kevin', 'Pineda', 16),
+('Rafael', 'Pineda', 17),
+('Flor', 'Fuentes', 19),
+('Flor', 'Fuentes', 20),
+('Merida', 'Caceres', 21),
+('Merida', 'Caceres', 22),
+('Kevin ', 'Ramirez', 23),
+('Maria', 'Valenzuela', 24),
+('Josefa', 'Sanchez', 26),
+('Maria ', 'Sanchez', 35),
+('Maria ', 'Sanchez', 36);
 
 -- --------------------------------------------------------
 
@@ -126,9 +143,8 @@ CREATE TABLE `relaciones` (
 --
 
 INSERT INTO `relaciones` (`id_profesor`, `id_materia`, `id_seccion`, `id_actividades`, `id_eventos`, `id`) VALUES
-(1, 1, 1, 1, 1, 1),
-(2, 4, 2, 1, 1, 2),
-(1, 2, 2, 1, 1, 3);
+(2, 6, 2, 2, 1, 4),
+(4, 3, 2, 2, 1, 6);
 
 -- --------------------------------------------------------
 
@@ -147,8 +163,35 @@ CREATE TABLE `secciones` (
 --
 
 INSERT INTO `secciones` (`nombre`, `periodoAcademico`, `id`) VALUES
-('VI', '2024-01-16', 1),
-('VII', '2024-01-16', 2);
+('VII', '2024-01-16', 2),
+('VIII', '2024-01-16', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `nombre` varchar(25) NOT NULL,
+  `user_name` varchar(25) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `tipo` varchar(25) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`nombre`, `user_name`, `password`, `tipo`, `id`) VALUES
+('Rafael', 'Rafael048', '1234', 'Profesor', 1),
+('Maria ', 'Maria123', '$2a$08$JnNnAmHMRx3Q.UB6UiNeSuP63xre48iXlYXQSn7YOedzZBIv2PYRa', 'Profesor', 3),
+('Mayra', 'Maya323', '$2a$08$4WwYRCJ1556D4TWx9GHzWOG55VGM.C/c2ldruXDDPg8qkx3zq7Wre', 'undefined', 6),
+('Jesus', 'Yisus0410', '$2a$08$5chG3Ne4KP6l9Jwryzdp0enHH.fTEH9fgPA/4zKgR2LKzL7JwSY82', 'director', 7),
+('Juan', 'Juan1234', '$2a$08$i/d.mosbBJOpqKnPXO/TYeYBB/MeYIDGjGW7IeFQNIccNXw.8EefO', 'profesor', 8),
+('mayra', 'mayra', '$2a$08$iNp1bpfHTEGPLFxL6Ik6f.SGGejhM2VSH8T1M6DZMIq7Ibo9tki0K', 'profesor', 12),
+('Eileen', 'Eli1234', '$2a$08$6jyaUB.A6IF3L745a/Ays.dvx/3dWUJuMN6dAODvzzIihTMjOaFIW', 'director', 14);
 
 --
 -- Índices para tablas volcadas
@@ -197,6 +240,13 @@ ALTER TABLE `secciones`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_name` (`user_name`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -204,37 +254,43 @@ ALTER TABLE `secciones`
 -- AUTO_INCREMENT de la tabla `actividades`
 --
 ALTER TABLE `actividades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `eventos`
 --
 ALTER TABLE `eventos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `materias`
 --
 ALTER TABLE `materias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `profesores`
 --
 ALTER TABLE `profesores`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT de la tabla `relaciones`
 --
 ALTER TABLE `relaciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `secciones`
 --
 ALTER TABLE `secciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Restricciones para tablas volcadas
