1c62b54c44af7a46875bf188d653e96a20a7eed6
Rafael <rafael2005perezmarin@gmail.com>
2024-03-14 11:25:34 -0400

    Solucionado error al subir a gitHub

--- controllers/RelacionesControladores.js
+++ controllers/RelacionesControladores.js
@@ -4,74 +4,87 @@ const RelacionesModelos = require('../models/RelacionesModelos')
 class RelacionesControladores{
     crear(datos){
         return new Promise((resolve,reject)=>{
-            let promesa = RelacionesModelos.crear(datos)
-            if(promesa){
-                resolve(promesa)
-            }else{
-                reject(e)
-            }
-        })
-       
+             RelacionesModelos.crear(datos)
+             .then(() => {
+                resolve()
+            })
+            .catch((error) => {
+                reject(error)
+            });
+        });
     }
+
     editar(id,datos){
         return new Promise((resolve,reject)=>{
-            let promesa = RelacionesModelos.editar(id,datos)
-            if(promesa){
-                resolve(promesa)
-            }else{
-                reject(e)
-            }
-        })
+            RelacionesModelos.editar(id,datos)
+            .then(() => {
+                resolve()
+            })
+            .catch((error) => {
+                reject(error)
+            });
+        });
     }
+
     profesores_materias(){
         return new Promise((resolve,reject)=>{
-            let promesa = RelacionesModelos.profesores_materias()
-            if(promesa){
-                resolve(promesa)
-            }else{
-                reject(e)
-            }
-        })
+            RelacionesModelos.profesores_materias()
+            .then((results) => {
+                resolve(results)
+            })
+            .catch((error) => {
+                reject(error)
+            });
+        });
     }
+
     profesores_materias_secciones(){
         return new Promise((resolve,reject)=>{
-            let promesa = RelacionesModelos.profesores_materias_secciones()
-            if(promesa){
-                resolve(promesa)
-            }else{
-                reject(e)
-            }
-        })
+            RelacionesModelos.profesores_materias_secciones()
+            .then((results) => {
+                resolve(results)
+            })
+            .catch((error) => {
+                reject(error)
+            });
+        });
     }
+
     secciones_eventos(){
         return new Promise((resolve,reject)=>{
-            let promesa = RelacionesModelos.secciones_eventos()
-            if(promesa){
-                resolve(promesa)
-            }else{
-                reject(e)
-            }
-        })
+            RelacionesModelos.secciones_eventos()
+             .then((results) => {
+                resolve(results)
+            })
+            .catch((error) => {
+                reject(error)
+            });
+        });
     }
+
     materias_actividades(){
         return new Promise((resolve,reject)=>{
-            let promesa = RelacionesModelos.materias_actividades()
-            if(promesa){
-                resolve(promesa)
-            }else{
-                reject(e)
-            }
-        })
+            RelacionesModelos.materias_actividades()
+            .then((results) => {
+                resolve(results)
+            })
+            .catch((error) => {
+                reject(error)
+            });
+        });
     }
+
     profesores_materias_secciones_actividades(){
         return new Promise((resolve,reject)=>{
-            let promesa = RelacionesModelos.profesores_materias_secciones_actividades()
-            if(promesa){
-                resolve(promesa)
-            }else{
-                reject(e)
-            }
-        })
+            RelacionesModelos.profesores_materias_secciones_actividades()
+            .then((resultados) => {
+                resolve(resultados)
+            })
+            .catch((error) => {
+                reject(error)
+            });
+        });
     }
 }
+
 module.exports=new RelacionesControladores();
