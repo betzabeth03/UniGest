5d8dc06a37d1615e0c31ec3a1077159e7eee599e
Rafael <rafael2005perezmarin@gmail.com>
2024-10-13 17:35:12 -0400

    Agregado el sistema para asignarle mateias a los profesores

---
+++ fronted/src/pages/AsingProfessor.jsx
@@ -0,0 +1,37 @@
+import {React,useState,useEffect} from 'react'
+import Header from '../components/Header'
+import '../css/FormActivitiesAdd.css'
+import axios from 'axios'
+import Cookies from 'js-cookie'
+import FormAsing from '../components/FormAsing'
+
+export default function FormActivitiesAdd() {
+  const activities = true
+  const token = Cookies.get('jwt')
+  const [active, setActive] = useState(null)
+  useEffect(() => {
+      async function getData(token) {
+          await axios.get(`http://localhost:3000/verify/${token}`)
+              .then((result) => {
+                  setActive(result.data.rol)
+              })
+              .catch((err) => {
+                  console.log(err)
+              })
+      }
+      getData(token)
+  }, [token])
+
+
+
+
+  return (
+    <div className='addActivities'>
+        <Header active={active} activities={activities}/>
+        <div className='addAct'>
+          <FormAsing
+          />        
+        </div>
+    </div>
+  )
+}
