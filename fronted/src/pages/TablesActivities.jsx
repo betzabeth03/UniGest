f752244c0869e4178a430f3d2b175abb42df0103
RicardoBRJ-Dev <ricardobriceno1134@gmail.com>
2024-10-13 14:51:48 -0400

    actividades

--- fronted/src/pages/TablesActivities.jsx
+++ fronted/src/pages/TablesActivities.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 import Tables from "../components/Tables";
+import '../css/TablesActivities.css'
+import axios from 'axios'
+import Cookies from 'js-cookie'
+
 
 export default function TablesActivities() {
+  const activities = true
+  const color = 'blue'
+  const token = Cookies.get('jwt')
+  const [active, setActive] = useState(null)
+  useEffect(() => {
+      async function getData(token) {
+          await axios.get(`http://localhost:3000/verify/${token}`)
+              .then((result) => {
+                  setActive(result.data.rol)
+              })
+              .catch((err) => {
+                  console.log(err)
+              })
+      }
+      getData(token)
+  }, [token])
+
   return (
-    <Tables
-    uri = "actividades"
-    />
+    <div>
+      <Header active={active} color={color} activities={activities}/>
+      <section className='tableActivities'>
+        <Tables
+          uri="actividades"
+        />
+
+      </section>
+      <Footer />
+    </div>
   )
 }
