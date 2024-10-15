6e92dfe2e417cde7ff95f75a5615b81fab1b23b6
Rafael <rafael2005perezmarin@gmail.com>
2024-10-14 20:15:40 -0400

    Arreglado el puto bug

--- fronted/src/components/Tables.jsx
+++ fronted/src/components/Tables.jsx
@@ -70,6 +70,7 @@ export default function Tables(props) {
     setShowX('notShowButton')
   }
   async function deleteElement(item) {
+    console.log(item.id)
     let id = item.id
     let verify = prompt("¿Realmente desea eliminar este elemento?(Y/N)")
     if (verify.toLowerCase() === "y") {
@@ -185,8 +186,8 @@ export default function Tables(props) {
                           <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
                         </svg>
                       </button>
-                      <button onClick={(e) => deleteElement(e)} className="tableButton">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="2vw" height="2vw" fill="red" className="bi bi-trash3-fill" viewBox="0 0 16 16">
+                      <button onClick={() => deleteElement(item)}   className="tableButton"> 
+                                               <svg xmlns="http://www.w3.org/2000/svg" width="2vw" height="2vw" fill="red" className="bi bi-trash3-fill" viewBox="0 0 16 16">
                           <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
                         </svg>
                       </button>
