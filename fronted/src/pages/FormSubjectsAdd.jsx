a7dd0471cc835df68f35747feb4e979a3d4be37d
RicardoBRJ-Dev <ricardobriceno1134@gmail.com>
2024-10-14 22:53:31 -0400

    2

--- fronted/src/pages/FormSubjectsAdd.jsx
+++ fronted/src/pages/FormSubjectsAdd.jsx
@@ -1,33 +1,13 @@
-import { React, useState, useEffect } from 'react'
+import { React} from 'react'
 import FormAdd from '../components/FormAdd'
 import Menu from '../components/Menu'
 import Footer from '../components/Footer'
 import '../css/FormActivitiesAdd.css'
-import axios from 'axios'
-import Cookies from 'js-cookie'
 import logo from '../assets/logo.png'
 
 
 export default function FormActivitiesAdd() {
   const btActive = 'activeMenu'
-  const token = Cookies.get('jwt')
-  const [active, setActive] = useState(null)
-  useEffect(() => {
-    async function getData(token) {
-      await axios.get(`http://localhost:3000/verify/${token}`)
-        .then((result) => {
-          setActive(result.data.user.rol)
-        })
-        .catch((err) => {
-          console.log(err)
-          window.location.replace('/')
-        })
-    }
-    getData(token)
-  }, [token])
-
-
-
 
   return (
     <>
